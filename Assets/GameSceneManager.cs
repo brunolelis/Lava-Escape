41690c423785f58dc8ec714efa71f71b9fce5ad9
Bruno Lelis <bleliscompany@gmail.com>
2021-07-22 18:22:59 -0300

    Adjust

--- Assets/GameSceneManager.cs
+++ Assets/GameSceneManager.cs
@@ -5,59 +5,59 @@ using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
-    [SerializeField] private GameObject highscore;
-    [SerializeField] private GameObject retry, menu;
+	[SerializeField] private GameObject highscore;
+	[SerializeField] private GameObject retry, menu;
 
-    public void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
-        highscore.SetActive(false);
-        Time.timeScale = 1f;
-        retry.SetActive(false);
-        menu.SetActive(false);
-    }
+		highscore.SetActive(false);
+		Time.timeScale = 1f;
+		retry.SetActive(false);
+		menu.SetActive(false);
+	}
 
-    public void Menu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(0);
-    }
+	public void Menu()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(0);
+	}
 
-    public void Play()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(1);
-    }
+	public void Play()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(1);
+	}
 
-    public void History()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(2);
-    }
+	public void History()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(2);
+	}
 
-    public void Time1()
-    {
-        Time.timeScale = 1f;
-    }
+	public void Time1()
+	{
+		Time.timeScale = 1f;
+	}
 
-    public void Time0()
-    {
-        Time.timeScale = 0f;
-    }
+	public void Time0()
+	{
+		Time.timeScale = 0f;
+	}
 
-    public void Tutorial()
-    {
-        SceneManager.LoadScene(3);
-    }
+	public void Tutorial()
+	{
+		SceneManager.LoadScene(3);
+	}
 
-    public void Hard()
-    {
-        SceneManager.LoadScene(4);
-    }
+	public void Hard()
+	{
+		SceneManager.LoadScene(4);
+	}
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 }
--- Assets/Lava/Lava.cs
+++ Assets/Lava/Lava.cs
@@ -6,66 +6,67 @@ using UnityEngine.SceneManagement;
 
 public class Lava : MonoBehaviour
 {
-    [SerializeField] private GameObject retry, menu;
+	[SerializeField] private GameObject retry, menu;
 
-    public float velocity = 2.5f;
+	public float velocity = 2.5f;
 
-    public AudioClip lavaClip, dieClip;
+	public AudioClip lavaClip, dieClip;
 
-    private void Update()
-    {
-        int score = LevelGenerator.GetLevelPartSpawned();
-        if(score >= 32)
-        {
-            velocity = 5f;
-        }
+	private void Update()
+	{
+		int score = LevelGenerator.GetLevelPartSpawned();
+		if (score >= 32)
+		{
+			velocity = 5f;
+		}
 
-        if(score <= 32)
-        {
-            if (score >= 17)
-            {
-                velocity = 3.8f;
-            }
-        }
+		if (score <= 32)
+		{
+			if (score >= 17)
+			{
+				velocity = 3.8f;
+			}
+		}
 
-        if(score == 17)
-        {
-            SoundManager.instance.PlayLava(lavaClip);
-        }
+		if (score == 17)
+		{
+			SoundManager.instance.PlayLava(lavaClip);
+		}
 
-        if(score == 32)
-        {
-            SoundManager.instance.PlayLava(lavaClip);
-        }
+		if (score == 32)
+		{
+			SoundManager.instance.PlayLava(lavaClip);
+		}
 
-        transform.position += Vector3.up * velocity * Time.deltaTime;
-    }
+		transform.position += Vector3.up * velocity * Time.deltaTime;
+	}
 
-    public void ChangeVelocity4()
-    {
-        velocity = 4f;
-    }
+	public void ChangeVelocity4()
+	{
+		velocity = 4f;
+	}
 
-    public void ChangeVelocity5()
-    {
-        velocity = 5f;
-    }
+	public void ChangeVelocity5()
+	{
+		velocity = 5f;
+	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.name == "Player")
-        {
-            SoundManager.instance.PlayDie(dieClip);
-            Time.timeScale = 0f;
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.name == "Player")
+		{
+			SoundManager.instance.PlayDie(dieClip);
+			Time.timeScale = 0f;
 
-            int finalScore = LevelGenerator.GetLevelPartSpawned();
-            HighscoreNameInputWindow.Show(finalScore, (string name) => {
-                HighscoreTable.AddHighscoreEntry_Static(finalScore, name);
-                HighscoreTable.Show();
+			int finalScore = LevelGenerator.GetLevelPartSpawned();
+			HighscoreNameInputWindow.Show(finalScore, (string name) =>
+			{
+				HighscoreTable.AddHighscoreEntry_Static(finalScore, name);
+				HighscoreTable.Show();
 
-                retry.SetActive(true);
-                menu.SetActive(true);
-            });
-        }
-    }
+				retry.SetActive(true);
+				menu.SetActive(true);
+			});
+		}
+	}
 }
